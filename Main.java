b63547aafbb547426fef57248ba96e40f4dc806e
Greg Loomis <gregory.loomis@fcps.org>
2025-09-25 13:12:37 -0400

    added 2 lines
    Not done yet, just added the 2 Sys.out print statements

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //Write your code here
-
-        
+        System.out.println("Hello Mr. Loomis");
+        System.out.println("new change");
     }
 }
